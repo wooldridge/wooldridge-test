10619da58579e3e9a8ddb4b874ee9917a70c3648
Mike Wooldridge <mike@hellion.org>
2017-01-26 16:02:56 -0800

    Add another comment

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var wooldridge = require('./index');
 // Say it in red
 wooldridge.sayItInRed('Alert!');
 
+// Say it in green
 wooldridge.sayItInGreen('All good.');
 
 wooldridge.sayItInBlue('Looks blue.');
